f511772029ed440a564c995498731513a3037298
Juan Luis Rodríguez <juanluisrp@gmail.com>
2021-06-25 10:51:37 +0200

    Publish artifacts in Maven repository

--- .space.kts
+++ .space.kts
@@ -7,9 +7,22 @@
 job("Build, test and install project artifacts") {
     
     container(displayName = "Run mvn install", image = "maven:latest") {
+        // url of a Space Packages repository
+        env["REPOSITORY_URL"] = "https://maven.pkg.jetbrains.space/geocat/p/jrc-inspire-portal/maven"
+
         shellScript {
             content = """
 	            mvn clean install -DskipTests
+            """
+        }
+        shellScript {
+            content = """
+                echo Publish artifacts...
+                mvn deploy -s .space/settings.xml \
+                    -DskipTests \
+                    -DrepositoryUrl=${'$'}REPOSITORY_URL \
+                    -DspaceUsername=${'$'}JB_SPACE_CLIENT_ID \
+                    -DspacePassword=${'$'}JB_SPACE_CLIENT_SECRET
             """
         }
     }
