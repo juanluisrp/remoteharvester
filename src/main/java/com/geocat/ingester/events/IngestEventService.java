d02e0297b5274e3d21f50c25898e2c0a7143e769
Jose García <josegar74@gmail.com>
2021-09-23 19:04:46 +0200

    Manage harvest COMPLETE status

--- src/main/java/com/geocat/ingester/events/IngestEventService.java
+++ src/main/java/com/geocat/ingester/events/IngestEventService.java
@@ -63,7 +63,7 @@ public class IngestEventService {
                 throw new Exception("Cannot find previous harvest run harvestJobId: " + harvestJobId);
             }
 
-            if (harvestJob.get().getState() != HarvestJobState.RECORDS_RECEIVED) {
+            if (harvestJob.get().getState() != HarvestJobState.COMPLETE) {
                 throw new Exception("Harvest run harvestJobId: " + harvestJobId + ", state '" + harvestJob.get().getState() + "' is not valid.");
             }
         }
