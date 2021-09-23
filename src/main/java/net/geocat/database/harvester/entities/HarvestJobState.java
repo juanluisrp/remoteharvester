3dbe0f5ff14956dea150a5d258f1aafe4286b8e9
Jose García <josegar74@gmail.com>
2021-09-23 17:54:32 +0200

    Add HarvestJobState value

--- src/main/java/net/geocat/database/harvester/entities/HarvestJobState.java
+++ src/main/java/net/geocat/database/harvester/entities/HarvestJobState.java
@@ -36,5 +36,7 @@ package net.geocat.database.harvester.entities;
 public enum HarvestJobState {
     CREATING,
     DETERMINING_WORK, WORK_DETERMINED, GETTING_RECORDS, RECORDS_RECEIVED,
-    ERROR, USERABORT
+    ERROR, USERABORT,
+
+    COMPLETE
 }
