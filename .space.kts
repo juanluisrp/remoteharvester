7969f06e2f680d691f770c5260a2edc7b9bee98b
Juan Luis Rodríguez Ponce <juanluisrp@gmail.com>
2022-02-22 10:55:55 +0100

    Build multi-platform images and publish them to ghcr.io (#5)
    * Build multi-platform images using Github Actions and publish them to Github Container Registry.
    * Don't publish images to ghcr.io from Space Automation
    * Use cross-platform compilation

--- .space.kts
+++ .space.kts
@@ -57,22 +57,15 @@ job("Build, test and install CSW-ingester artifacts") {
         env["GEOCAT_DOCKER_REGISTRY_URL"] = "docker-registry.geocat.net:5000"
         env["GEOCAT_DOCKER_REGISTRY_USER"] = Params("geocat_docker_registry_user")
         env["GEOCAT_DOCKER_REGISTRY_PASSWORD"] = Secrets("geocat_docker_registry_password")
-        env["GITHUB_REGISTRY_USERNAME"] = Params("github_registry_username")
-        env["GITHUB_REGISTRY_PAT"] = Secrets("github_registry_pat")
         
         shellScript {
             content = """
             	BRANCH=${'$'}(echo ${'$'}JB_SPACE_GIT_BRANCH | cut -d'/' -f 3)
                 crane auth login geocat.registry.jetbrains.space -u ${'$'}JB_SPACE_CLIENT_ID -p ${'$'}JB_SPACE_CLIENT_SECRET
                 crane auth login ${'$'}GEOCAT_DOCKER_REGISTRY_URL -u ${'$'}GEOCAT_DOCKER_REGISTRY_USER -p ${'$'}GEOCAT_DOCKER_REGISTRY_PASSWORD
-                crane auth login ghcr.io -u ${'$'}GITHUB_REGISTRY_USERNAME -p ${'$'}GITHUB_REGISTRY_PAT
-
                 
 				crane copy geocat.registry.jetbrains.space/p/jrc-inspire-portal/docker/csw-ingester:${'$'}BRANCH-${'$'}JB_SPACE_EXECUTION_NUMBER ${'$'}GEOCAT_DOCKER_REGISTRY_URL/jrc-inspire-portal/csw-ingester:${'$'}BRANCH-${'$'}JB_SPACE_EXECUTION_NUMBER
 				crane copy geocat.registry.jetbrains.space/p/jrc-inspire-portal/docker/csw-ingester:${'$'}BRANCH-${'$'}JB_SPACE_EXECUTION_NUMBER ${'$'}GEOCAT_DOCKER_REGISTRY_URL/jrc-inspire-portal/csw-ingester:${'$'}BRANCH
-
-                crane copy geocat.registry.jetbrains.space/p/jrc-inspire-portal/docker/csw-ingester:${'$'}BRANCH-${'$'}JB_SPACE_EXECUTION_NUMBER ghcr.io/geocat/csw-ingester:${'$'}BRANCH-${'$'}JB_SPACE_EXECUTION_NUMBER
-				crane copy geocat.registry.jetbrains.space/p/jrc-inspire-portal/docker/csw-ingester:${'$'}BRANCH-${'$'}JB_SPACE_EXECUTION_NUMBER ghcr.io/geocat/csw-ingester:${'$'}BRANCH
 			"""
         }
     }
--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,26 @@
-FROM maven:3-eclipse-temurin-8 as builder
+FROM --platform=$BUILDPLATFORM maven:3-eclipse-temurin-8 as builder
 
 
 COPY ./pom.xml ./pom.xml
 # store maven dependencies so next build doesn't have to download them again
-RUN mvn dependency:go-offline -B
+RUN --mount=type=cache,target=/root/.m2/repository \
+    mvn dependency:go-offline -B
 
 COPY ./src ./src
-RUN mvn -B package -DskipTests
+
+RUN --mount=type=cache,target=/root/.m2/repository \
+    mvn -B package -DskipTests
 RUN mkdir /application && \
     cp target/*.jar /application/csw-ingester.jar
 
 WORKDIR /application
 
 # Extract spring boot JAR layers
-RUN java -Djarmode=layertools -jar csw-ingester.jar extract
+RUN --mount=type=cache,target=/root/.m2/repository \ 
+    java -Djarmode=layertools -jar csw-ingester.jar extract
 
 
-FROM eclipse-temurin:8-jre
+FROM eclipse-temurin:8-jre  as finalImage
 
 LABEL vendor="GeoCat B.V."
 LABEL org.opencontainers.image.source https://github.com/GeoCat/csw-ingester
@@ -26,7 +30,6 @@ LABEL org.opencontainers.image.source https://github.com/GeoCat/csw-ingester
 # The property names can be translated to environment varibles passing them to upper case and replacing the dots
 # with underscores. For example harvester.jdbc.url -> HARVESTER_JDBC_URL
 
-RUN mkdir -p /opt/jrc-ingester
 WORKDIR /opt/jrc-ingester
 
 COPY --from=builder /application/dependencies/ ./
